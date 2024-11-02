be2ab0960732dcb6b305f0d7dd964e623b65f36c
Kanon dev <kanon2017sengrem@gmail.com>
2024-11-02 17:55:41 +0600

    Banner section added

---
+++ src/components/DashBord/DashBord.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashBord = () => {
+    return (
+        <div>
+            <h2 className='text-5xl'>This is DashBord</h2>
+        </div>
+    );
+};
+
+export default DashBord;
